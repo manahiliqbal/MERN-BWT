6e5ae818257532f3830aaffd4f5b102b6e24513f
manahil <120031852+manahiliqbal@users.noreply.github.com>
2024-08-04 12:37:37 +0500

    Chat Application

--- week 8/backend/server.js
+++ week 8/backend/server.js
@@ -2,10 +2,13 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
+
 import { app, server } from "./socket/socket.js";
 
 dotenv.config();
